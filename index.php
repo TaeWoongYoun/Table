298f9ae84718cbe17faeb638ad428ad6fd198360
xodnd0507 <24457545yong@gmail.com>
2024-05-22 21:57:58 +0900

    feat : Table

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>표 만들기</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<table>
+    <tr>
+        <th scope="col">이름</td>
+        <th scope="col">소속</td>
+        <th scope="col">특이사항</td>
+    </tr>
+    <tr>
+        <td>윤도연</td>
+        <td>방송부</td>
+        <td>여자에 미침</td>
+    </tr>
+    <tr>
+        <td>원필립</td>
+        <td>기능반</td>
+        <td>유령회원</td>
+    </tr>
+</table>
+</body>
+</html>
