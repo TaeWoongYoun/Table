7ccfbfc95756b0ac73e165e1413481039258f8ff
xodnd0507 <24457545yong@gmail.com>
2024-06-01 21:19:20 +0900

    test : Join

--- join.php
+++ join.php
@@ -4,29 +4,24 @@
     $userid = mysqli_real_escape_string($conn, $_POST['joinid']);
     $userpw = mysqli_real_escape_string($conn, $_POST['joinpw']);
 
-
     $sql = "INSERT INTO user_join (userid, password) VALUE('$userid', '$userpw')";
     mysqli_query($conn, $sql);
-    // echo $select = "SELECT * FROM user_join WHERE userid='$userid'";
-    // $result = mysqli_query($conn, $select);
-    // echo $result;
 
-    
-
-
-    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //     $userid = mysqli_real_escape_string($conn, $_POST['joinid']);
-    //     $select = "SELECT * FROM user_join WHERE userid='$userid'";
-    //     $result = mysqli_query($conn, $select);
-    //     echo mysqli_num_rows($result);
-    //     if (mysqli_num_rows($result) > 0) {
-    //         echo "<script>alert('이미 사용중인 아이디입니다.');</script>";
-    //     } else {
-    //         // $sql = "INSERT INTO user_join (userid, password) VALUES ('$userid', '$userpw')";
-    //         // mysqli_query($conn, $sql);
-    //         echo "<script>alert('사용 가능한 아이디입니다.');</script>";
-    //     }
-    // }
+    echo $select = "SELECT * FROM user_join WHERE userid='$userid'";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo $userid = mysqli_real_escape_string($conn, $_POST['joinid']);
+        echo $select = "SELECT * FROM user_join WHERE userid='$userid'";
+        echo $result = mysqli_query($conn, $select);
+        if (mysqli_num_rows($result) > 0) {
+            echo "<script>
+                    alert('이미 사용중인 아이디입니다.');
+                </script>";
+        } else {
+            echo "<script>
+                    alert('사용 가능한 아이디입니다.');
+                </script>";
+        }
+    }
 ?>
 
 <script>
