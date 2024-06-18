037693c7ba7d09db8aafd342381087c12e5c2a0c
xodnd0507 <24457545yong@gmail.com>
2024-06-19 07:55:13 +0900

    fix : Array code

--- updateProcess.php
+++ updateProcess.php
@@ -1,12 +1,11 @@
 <?php $conn = mysqli_connect('localhost', 'root', '', 'adminDB')?>
 <?php
-    $filter = array(
-        'id'=>mysqli_real_escape_string($conn, $_POST['id']),
-        'name'=>mysqli_real_escape_string($conn, $_POST['name']),
-        'city'=>mysqli_real_escape_string($conn, $_POST['city'])
-    );
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $city = mysqli_real_escape_string($conn, $_POST['city']);
 
-    $sql = "UPDATE adminTable SET name = '{$filter['name']}', city = '{$filter['city']}' WHERE id = {$filter['id']}";
+
+    $sql = "UPDATE adminTable SET name = '$name', city = '$city' WHERE id = $id";
     $result = mysqli_query($conn, $sql);
 ?>
 
